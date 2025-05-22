741cdd0e8b14277c6785a3dca05dab8454bf3377
smonolo <lu3mtex@gmail.com>
2025-05-22 10:40:33 +0200

    feat(browser): add support for scrolling with arrow keys

--- browser.py
+++ browser.py
@@ -5,6 +5,8 @@ from url import URL
 
 
 WIDTH, HEIGHT = 800, 600
+HSTEP, VSTEP = 13, 18
+SCROLL_STEP = 100
 
 
 def lex(body: str):
@@ -24,25 +26,55 @@ def lex(body: str):
     return text
 
 
+def layout(text: str):
+    display_list = []
+    cursor_x, cursor_y = HSTEP, VSTEP
+
+    for c in text:
+        display_list.append((cursor_x, cursor_y, c))
+        cursor_x += HSTEP
+
+        if cursor_x >= WIDTH - HSTEP:
+            cursor_y += VSTEP
+            cursor_x = HSTEP
+
+    return display_list
+
+
 class Browser:
     def __init__(self):
         self.window = tkinter.Tk()
         self.canvas = tkinter.Canvas(self.window, width=WIDTH, height=HEIGHT)
         self.canvas.pack()
 
+        self.scroll = 0
+        self.window.bind("<Down>", self.scroll_down)
+        self.window.bind("<Up>", self.scroll_up)
+
     def load(self, url: URL):
         body = url.request()
         text = lex(body)
-        HSTEP, VSTEP = 13, 18
-        cursor_x, cursor_y = HSTEP, VSTEP
+        self.display_list = layout(text)
+        self.draw()
 
-        for c in text:
-            self.canvas.create_text(cursor_x, cursor_y, text=c)
-            cursor_x += HSTEP
-            
-            if cursor_x >= WIDTH - HSTEP:
-                cursor_y += VSTEP
-                cursor_x = HSTEP
+    def draw(self):
+        self.canvas.delete("all")
+
+        for x, y, c in self.display_list:
+            if y > self.scroll + HEIGHT:
+                continue
+            if y + VSTEP < self.scroll:
+                continue
+
+            self.canvas.create_text(x, y - self.scroll, text=c)
+
+    def scroll_down(self, _):
+        self.scroll += SCROLL_STEP
+        self.draw()
+
+    def scroll_up(self, _):
+        self.scroll -= SCROLL_STEP
+        self.draw()
 
 
 if __name__ == "__main__":
